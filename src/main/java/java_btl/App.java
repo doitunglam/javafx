19eb267293050104d9a25b19f29209bfd46fe315
Doi Tung Lam <doitunglam1@gmail.com>
2022-06-06 11:14:01 +0700

    bruh

--- src/main/java/java_btl/App.java
+++ src/main/java/java_btl/App.java
@@ -5,79 +5,29 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.Group;
 import javafx.stage.Stage;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
-    private static Scene scene;
-    private double screenHeight, screenWidth;
-    //
+    
     //Nhap mang(2) -> Xu ly mang + Sap xep Animation(2) 
     @Override 
     public void start(Stage stage) throws IOException {
-        this.screenHeight = 768.0;
-        this.screenWidth = 1024.0;
         ArrayList<Integer> init = new ArrayList<Integer>();
-        // Nhap xuat du lieu 
         Random rand = new Random();
         for (Integer i = 0; i < 15; i++)
             init.add(rand.nextInt(100));
+
+        MainScene mainScene = new MainScene(new Group(),1024,728);
         MainArray mainArray = new MainArray(init);
-        mainArray.setWindowSize(screenWidth, screenHeight);
-        mainArray.render();
-        //Khoi thuat toan sap xep
-        AnimationQueue newQueue = new AnimationQueue();
-        newQueue.add(mainArray.moveIndicatorTo(mainArray.primaryIndicator, 0));
-        for (int i = 0; i < 14; i++) {
-             if (i == 0)
-                 newQueue.add(mainArray.primaryIndicator.makeAppear());
-             else
-                 newQueue.add(mainArray.moveIndicatorTo(mainArray.primaryIndicator, i));
-             newQueue.add(mainArray.moveIndicatorTo(mainArray.secondaryIndicator, i+1));
-            for (int j = i + 1; j < 15; j++) {
-                 if (j == i + 1)
-                     newQueue.add(mainArray.secondaryIndicator.makeAppear());
-                 else
-                    newQueue.add(mainArray.moveIndicatorTo(mainArray.secondaryIndicator, j));
-                if (mainArray.get(i).getKey() < mainArray.get(j).getKey())
-                    newQueue.add(mainArray.swap(i, j));
-            }
-            newQueue.add(mainArray.secondaryIndicator.makeDisappear());
-        }
-        newQueue.add(mainArray.primaryIndicator.makeDisappear());
-        newQueue.setCompleted(true);
-        //AnimationQueue day du
-        Button playButton = new Button();
-        Button pauseButton = new Button();
-        pauseButton.setDisable(true);
-        pauseButton.setText("Pause");
-        playButton.setText("Play");
-        playButton.setOnMouseClicked(Event -> {
-            playButton.setDisable(true);
-            pauseButton.setDisable(false);
-            newQueue.resetInteruputed();
-        });
-        pauseButton.setOnMouseClicked(Event ->{
-            pauseButton.setDisable(true);
-            playButton.setDisable(false);
-            newQueue.setInterupted();
-           
-        });
-        AnchorPane ap = new AnchorPane();
-        FlowPane fp = new FlowPane();
-        fp.getChildren().addAll(playButton,pauseButton);
-        ap.getChildren().add(fp);
-        ap.getChildren().add(mainArray.renderedArray);
-        ap.getChildren().addAll(mainArray.primaryIndicator, mainArray.secondaryIndicator);
-        scene = new Scene(ap, screenWidth, screenHeight);
-        stage.setScene(scene);
+        mainScene.setMainArray(mainArray);
+        mainScene.render();
+        mainScene.setAnimationQueue();
+        stage.setScene(mainScene);
+        stage.setTitle("Demo Sorting");
         stage.show();
     }
 
---
+++ src/main/java/java_btl/MainScene.java
@@ -0,0 +1,95 @@
+package java_btl;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+
+public class MainScene extends Scene {
+
+    private MainArray mainArray;
+    private AnimationQueue animationQueue;
+
+    public MainScene(Parent arg0, double arg1, double arg2) {
+        super(arg0, arg1, arg2);
+        animationQueue = new AnimationQueue();
+    }
+
+    public MainArray getMainArray() {
+        return mainArray;
+    }
+
+    public AnimationQueue getAnimationQueue() {
+        return animationQueue;
+    }
+
+    public void setMainArray(MainArray mainArray) {
+        this.mainArray = mainArray;
+    }
+
+    public void render() {
+        mainArray.setWindowSize(this.getWidth(), this.getHeight());
+        mainArray.render();
+
+        Button pauseButton = new Button();
+        Button playButton = new Button();
+        Button exitButton = new Button();
+
+        pauseButton.setDisable(true);
+        pauseButton.setText("Pause");
+        pauseButton.setOnMouseClicked(Event -> {
+            pauseButton.setDisable(true);
+            playButton.setDisable(false);
+            animationQueue.setInterupted();
+        });
+
+        playButton.setText("Play");
+        playButton.setOnMouseClicked(Event -> {
+            playButton.setDisable(true);
+            pauseButton.setDisable(false);
+            animationQueue.resetInteruputed();
+        });
+
+        exitButton.setOnMouseClicked(Event -> {
+            this.setRoot(new Parent() {
+            });
+        });
+
+        FlowPane fp = new FlowPane();
+        fp.getChildren().addAll(playButton, pauseButton);
+        fp.setHgap(10.0);
+        fp.setAlignment(Pos.CENTER);
+
+        BorderPane bp = new BorderPane();
+        bp.setBottom(fp);
+        bp.setPadding(new Insets(100));
+        bp.getChildren().add(mainArray.renderedArray);
+        bp.getChildren().addAll(mainArray.primaryIndicator, mainArray.secondaryIndicator);
+        this.setRoot(bp);
+    }
+
+    public void setAnimationQueue() {
+        animationQueue.add(mainArray.moveIndicatorTo(mainArray.primaryIndicator, 0));
+        for (int i = 0; i < 14; i++) {
+            if (i == 0)
+                animationQueue.add(mainArray.primaryIndicator.makeAppear());
+            else
+                animationQueue.add(mainArray.moveIndicatorTo(mainArray.primaryIndicator, i));
+            animationQueue.add(mainArray.moveIndicatorTo(mainArray.secondaryIndicator, i + 1));
+            for (int j = i + 1; j < 15; j++) {
+                if (j == i + 1)
+                    animationQueue.add(mainArray.secondaryIndicator.makeAppear());
+                else
+                    animationQueue.add(mainArray.moveIndicatorTo(mainArray.secondaryIndicator, j));
+                if (mainArray.get(i).getKey() < mainArray.get(j).getKey())
+                    animationQueue.add(mainArray.swap(i, j));
+            }
+            animationQueue.add(mainArray.secondaryIndicator.makeDisappear());
+        }
+        animationQueue.add(mainArray.primaryIndicator.makeDisappear());
+        animationQueue.setCompleted(true);
+    }
+}
